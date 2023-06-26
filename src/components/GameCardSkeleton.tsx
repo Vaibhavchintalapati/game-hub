0a25caad7876e7423ba511e33b70c25d656f3760
Chintalapati Abhijith Vaibhav <chintalapati.abhijith-vaibhav@capgemini.com>
2023-06-26 10:28:13 +0530

    Refactor: Remove duplicated styles

--- src/components/GameCardSkeleton.tsx
+++ src/components/GameCardSkeleton.tsx
@@ -3,7 +3,7 @@ import { Card, Skeleton, CardBody, SkeletonText } from "@chakra-ui/react";
 function GameCardSkeleton() {
   return (
     <>
-      <Card width="250px" borderRadius={10} overflow="hidden">
+      <Card>
         <Skeleton height="200px" />
         <CardBody>
           <SkeletonText />
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -5,6 +5,7 @@ import { Text, SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
 import GameCardSkeleton from "./GameCardSkeleton";
 
+import GameCardContainer from "./GameCardContainer";
 function GameGrid() {
   const { games, error, isLoading } = useGames();
   let skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -15,9 +16,17 @@ function GameGrid() {
         spacing={10}
         padding="10px"
       >
-        {isLoading && skeletons.map((item) => <GameCardSkeleton key={item} />)}
+        {isLoading &&
+          skeletons.map((item) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={item} />
+            </GameCardContainer>
+          ))}
         {games.map((game) => (
-          <GameCard key={game.id} game={game}></GameCard>
+          <GameCardContainer>
+            {" "}
+            <GameCard key={game.id} game={game}></GameCard>{" "}
+          </GameCardContainer>
         ))}
       </SimpleGrid>
       {error && <Text>{error}</Text>}
